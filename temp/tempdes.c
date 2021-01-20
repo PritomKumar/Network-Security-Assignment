bf89f71d5b9c0030c27021f1319a519ae3ec8da4
Pritom Kumar <bsse0919@iit.du.ac.bd>
2021-01-20 18:28:46 +0600

    Trying to intgrate

--- temp/tempdes.c
+++ temp/tempdes.c
@@ -12,26 +12,26 @@ int hex_to_int(char c)
 	int dig = 0;
 	switch (c)
 	{
-		case 'A':
-			dig = 10;
-			break;
-		case 'B':
-			dig = 11;
-			break;
-		case 'C':
-			dig = 12;
-			break;
-		case 'D':
-			dig = 13;
-			break;
-		case 'E':
-			dig = 14;
-			break;
-		case 'F':
-			dig = 15;
-			break;
-		default:
-			dig = c - 0x30;
+	case 'A':
+		dig = 10;
+		break;
+	case 'B':
+		dig = 11;
+		break;
+	case 'C':
+		dig = 12;
+		break;
+	case 'D':
+		dig = 13;
+		break;
+	case 'E':
+		dig = 14;
+		break;
+	case 'F':
+		dig = 15;
+		break;
+	default:
+		dig = c - 0x30;
 	}
 	return dig;
 }
@@ -42,6 +42,30 @@ int hex_to_ascii(char c, char d)
 	int low = hex_to_int(d);
 	// printf("\nhigh = %d low = %d  \n", high, low);
 	return high + low;
+}
+unsigned char *hexToAsciiString(char *hexString)
+{
+	char *st = hexString;
+	unsigned char ascii[8];
+	int length = strlen(st);
+	int i;
+	char buf = st[0];
+	for (int i = 0, j = 0; i < length - 1; i += 2, j++)
+	{
+		// printf( "buf =  %c\n ",buf);
+		// printf("%c", (unsigned char)hex_to_ascii(st[i], st[i + 1]));
+		// printf(" %d ", hex_to_ascii(buf, st[i]));
+		unsigned char c = (unsigned char)hex_to_ascii(st[i], st[i + 1]);
+		ascii[j] = c;
+	}
+	// printf("\n");
+	// for (int i = 0; i < 8; i++)
+	// {
+	// 	printf("%c", ascii[i]);
+	// }
+	unsigned char *p;
+	p = ascii;
+	return p;
 }
 
 int main(int argc, char *argv[])
@@ -84,6 +108,7 @@ int main(int argc, char *argv[])
 	}
 	printf("\n");
 
+	unsigned char * iv = hexToAsciiString(argv[1]);
 	// 	int i=0;
 	// 	printf("\ncmdline args count=%d", argc);
 
@@ -93,23 +118,6 @@ int main(int argc, char *argv[])
 	// 	for (i=1; i< argc; i++) {
 	// 		printf("\narg%d=%s", i, argv[i]);
 	// 	}
-	const char *st = "6861686168616861";
-	unsigned char ascii[8];
-	int length = strlen(st);
-	int i;
-	char buf = st[0];
-	for (int i = 0,  j=0; i < length - 1; i += 2, j++)
-	{
-		// printf( "buf =  %c\n ",buf);
-		// printf("%c", (unsigned char)hex_to_ascii(st[i], st[i + 1]));
-		// printf(" %d ", hex_to_ascii(buf, st[i]));
-		unsigned char c = (unsigned char)hex_to_ascii(st[i], st[i + 1]);
-		ascii[j] = c;
-	}
-	// printf("\n");
-	// for (int i = 0; i < 8; i++)
-	// {
-	// 	printf("%c", ascii[i]);
-	// }
-	printf("\n");
+
+	printf("%s\n",iv);
 }
